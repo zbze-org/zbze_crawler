a0192805b34081e587b95e7d689c409f11608260
panagoa <panagoa@ya.ru>
2023-12-20 14:05:39 +0300

    init scrapy

---
+++ zbze_scrapy/zbze_scrapy/spiders/elgkbr_ru.py
@@ -0,0 +1,66 @@
+import os
+from urllib.parse import urlparse
+
+from scrapy.exporters import JsonLinesItemExporter
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+from tinydb import TinyDB, Query
+
+
+class ElgkbrRuSpider(CrawlSpider):
+    name = 'elgkbr_ru'
+    allowed_domains = ['elgkbr.ru']
+    start_urls = ['https://elgkbr.ru/newskab']
+
+    rules = (
+        Rule(LinkExtractor(allow=r'/node'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(allow=r'\?page=\d+'), follow=True),
+    )
+
+    def __init__(self, *args, **kwargs):
+        super(ElgkbrRuSpider, self).__init__(*args, **kwargs)
+        self.dump_dir = f'../data/{self.name}'
+        os.makedirs(self.dump_dir, exist_ok=True)
+
+        self.file = open(os.path.join(self.dump_dir, 'elgkbr_ru.jl'), 'wb')
+        self.exporter = JsonLinesItemExporter(self.file, encoding='utf-8', ensure_ascii=False)
+        self.exporter.start_exporting()
+
+        self.db = TinyDB(os.path.join(self.dump_dir, 'elgkbr_ru.json'))
+        self.query = Query()
+
+    def close_spider(self, spider):
+        self.exporter.finish_exporting()
+        self.file.close()
+
+    def dump_file(self, response):
+        url_path = '__'.join(urlparse(response.url).path.split('/'))
+        with open(os.path.join(self.dump_dir, f'{url_path}.html'), 'wb') as html_file:
+            html_file.write(response.body)
+
+    def save_to_db(self, document):
+        self.db.upsert(document, self.query.url == document['url'])
+
+    def parse_item(self, response):
+        category = response.css('div.field-name-field-rubrika-kab a::attr(href)').get()
+        if category != '/newskab':
+            return
+
+        title = response.css('h1.title::text').get()
+        publication_date = response.css('div.meta.submitted span::attr(content)').get()
+        content = ''.join(response.css('div.field-name-body ::text').getall())
+        author = response.css('div.field-name-field-author .field-item::text').get()
+
+        item = {
+            'url': response.url,
+            'title': title.strip() if title else None,
+            'publication_date': publication_date.strip() if publication_date else None,
+            'content': content.strip() if content else None,
+            'author': author.strip() if author else None,
+        }
+
+        self.exporter.export_item(item)
+        # self.dump_file(response)
+        self.save_to_db(item)
+        return item
+
